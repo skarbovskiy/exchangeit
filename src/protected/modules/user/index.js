459a8601daef38eddae08a02f680e100e666d539
skarbovskiy <george.skarbovskiy@elastic.io>
2015-04-23 22:05:37 +0300

    added auth service

---
+++ src/protected/modules/user/index.js
@@ -0,0 +1,71 @@
+'use strict';
+var pg = require('../core/bootstrap').get('pg');
+var crypto = require('crypto');
+
+var salt = 'wef*(&hfwjekfh*0flm';
+/*
+ id serial NOT NULL,
+ phone text,
+ password text,
+ status text, [not-activated, active, blocked. deleted]
+ type text, [normal, admin]
+ comment text,
+ created_date timestamp with time zone DEFAULT now(),
+ */
+var User = {
+    register: function (phone, password, callback) {
+        if (!(/\+[0-9]{10,13}/.test(phone)) || !password) {
+            var error = new Error('bad phone or password passed');
+            error.status = 422;
+            return callback(error);
+        }
+        pg(function (client, done) {
+            client.setQuery(
+                'INSERT INTO users.users (phone, password, status, type) ' +
+                'VALUES ($1, $2, $3, $4)',
+                [phone, helper.md5(password + salt), 'active', 'normal'],
+                function (error) {
+                    done();
+                    if (error && /duplicate/.test(error.message)) {
+                        error = new Error('user with this phone already exists');
+                        error.status = 422;
+                    }
+                    callback(error);
+                }
+            )
+        });
+    },
+    login: function (phone, password, callback) {
+        pg(function (client, done) {
+            client.setQuery(
+                'SELECT * FROM users.users AS u ' +
+                'LEFT JOIN users.user_properties AS p ON p.user_id = u.id ' +
+                'WHERE u.phone = $1 AND u.password = $2',
+                [phone, helper.md5(password + salt)],
+                function (error, data) {
+                    done();
+                    if (error) {
+                        return callback(error);
+                    }
+                    if (data.rows.length === 0) {
+                        var error = new Error('no user found');
+                        error.status = 400;
+                        return callback(error);
+                    }
+                    callback(null, data.rows[0]);
+                }
+            )
+        });
+    }
+};
+
+
+var helper = {
+    md5: function (string) {
+        var md5sum = crypto.createHash('md5');
+        md5sum.update(string);
+        return md5sum.digest('hex');
+    }
+}
+
+module.exports = User;
--- src/protected/services/user/authentication.js
+++ src/protected/services/user/authentication.js
@@ -1,19 +1,53 @@
 'use strict';
 
 var sessionHandler = require('../../modules/core/sessionHandler');
+var userModel = require('../../modules/user/index');
 
 var Service = {
+    /**
+     * session generation service needed for every (even non-authenticated) requests
+     * @param request
+     * @param response
+     */
     get_token: function (request, response) {
         sessionHandler.create(request.ip, {}, function (error, token) {
             response(error, {token: token});
 
         });
     },
-    get_token2: function (request, response) {
-        var session = request.session;
-        console.log(session);
-        request.session.set({testdat2a: null}, function (error) {
-            response(error, {test2: true});
+    /**
+     * authenticate user via given login and password and store data in session
+     * @param request
+     * @param response
+     */
+    login: function (request, response) {
+        if (request.session.user && request.session.user.id) {
+            var error = new Error('already authenticated');
+            error.status = 409;
+            return response(error);
+        }
+        userModel.login(request.body.phone, request.body.password, function (error, user) {
+            if (error) {
+                return response(error);
+            }
+            request.session.set({user: user}, function (error) {
+                response(error);
+            })
+        });
+    },
+
+    test: function (request, response) {
+        response(null, request.session);
+    },
+
+    register: function (request, response) {
+        if (request.session.user && request.session.user.id) {
+            var error = new Error('already authenticated');
+            error.status = 409;
+            return response(error);
+        }
+        userModel.register(request.body.phone, request.body.password, function (error) {
+            response(error);
         });
     }
 };
